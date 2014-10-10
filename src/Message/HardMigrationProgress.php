060e267983c1e128101bb04d395b66d569c9ecd6
Artem Naumenko <an@whotrades.org>
2014-10-10 11:04:59 +0400

    [+] First version of hard migrations added #WTA-69

---
+++ src/Message/HardMigrationProgress.php
@@ -0,0 +1,17 @@
+<?php
+namespace RdsSystem\Message;
+
+class HardMigrationProgress extends Base
+{
+    public $migration;
+    public $progress;
+    public $pid;
+
+    public function __construct($migration, $progress, $action, $pid)
+    {
+        $this->migration = $migration;
+        $this->progress = $progress;
+        $this->action = $action;
+        $this->pid = $pid;
+    }
+}
