1e72c2611dd36f6ca0a142357b981434ce19f79f
Artem Naumenko <an@whotrades.org>
2015-01-29 13:59:53 +0300

    [+] Added sending migration error message to RDS  #WTA-91

--- src/Message/ReleaseRequestMigrationStatus.php
+++ src/Message/ReleaseRequestMigrationStatus.php
@@ -7,12 +7,14 @@ class ReleaseRequestMigrationStatus extends Base
     public $version;
     public $type;
     public $status;
+    public $errorText;
 
-    public function __construct($project, $version, $type, $status)
+    public function __construct($project, $version, $type, $status, $errorText = "")
     {
         $this->project = $project;
         $this->version = $version;
         $this->type = $type;
         $this->status = $status;
+        $this->errorText = $errorText;
     }
 }
