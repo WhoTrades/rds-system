b40f03a8f357139eb24d3e2c2c052a0ab7cc7e98
Dmitry Glizhinskiy <dg@whotrades.org>
2016-01-15 11:20:19 +0300

    [+] add PhpSyntaxValidator #WTA-568

--- src/lib/CommandExecutorException.php
+++ src/lib/CommandExecutorException.php
@@ -5,10 +5,26 @@ class CommandExecutorException extends \ApplicationException
 {
     public $output;
 
+    /**
+     * CommandExecutorException constructor.
+     *
+     * @param string|null $message
+     * @param int $code
+     * @param string $output
+     * @param \Exception $previous
+     */
     public function __construct($message, $code, $output, $previous = null)
     {
         $this->output = $output;
         parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutput()
+    {
+        return $this->output;
     }
 }
 
