67d31b27e588ae1bcbfe83a357c86d0b7c8cee1e
Artem Naumenko <an@whotrades.org>
2016-02-20 17:16:25 +0300

    [+] Added support of multiple config files #WTA-605

--- src/Message/ProjectConfig.php
+++ src/Message/ProjectConfig.php
@@ -1,14 +1,31 @@
 <?php
+/**
+ * @author Artem Naumenko
+ *
+ * Сообщение-задача на обновление локальных конфигов у проекта. Сообщение генерируется RDS и обрабатывается сервисом deploy
+ */
 namespace RdsSystem\Message;
 
 class ProjectConfig extends Base
 {
     public $project;
-    public $config;
+    public $configs = [];
 
-    public function __construct($project, $config)
+    /**
+     * ProjectConfig constructor.
+     *
+     * @param string $project - имя проекта, в котором мы будем обновлять конфигурацию
+     * @param array  $configs - список конфиг-файлов с содержимым. Формат: array(
+     *    'config.local.php' => '<?php ...',
+     *    'config2.local.php' => '<?php ...',
+     *    'config3.local.php' => '<?php ...',
+     * )
+     */
+    public function __construct($project, array $configs)
     {
         $this->project  = $project;
-        $this->config   = $config;
+        $this->configs   = $configs;
+
+        parent::__construct();
     }
 }
