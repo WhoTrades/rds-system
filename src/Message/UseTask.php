74a048e332ddac24197b29e4818734537d2acff0
Artem Naumenko <an@whotrades.org>
2016-07-18 14:46:00 +0300

    [+] Code style fixes and some refactoring. Status used_attempt removed from release_request #WTA-743

--- src/Message/UseTask.php
+++ src/Message/UseTask.php
@@ -6,13 +6,23 @@ class UseTask extends Base
     public $project;
     public $releaseRequestId;
     public $version;
-    public $useStatus;
+    public $initiatorUserName;
 
-    public function __construct($project, $releaseRequestId, $version, $useStatus)
+    /**
+     * UseTask constructor.
+     *
+     * @param string $project - название проекта, например comon, service-crm
+     * @param int $releaseRequestId comon4.rds.release_request.obj_id
+     * @param string $version - например 67.00.12.1289
+     * @param string $initiatorUserName - имя того, кто нажал use
+     */
+    public function __construct($project, $releaseRequestId, $version, $initiatorUserName)
     {
         $this->project = $project;
         $this->releaseRequestId = $releaseRequestId;
         $this->version = $version;
-        $this->useStatus = $useStatus;
+        $this->initiatorUserName = $initiatorUserName;
+
+        parent::__construct();
     }
 }
