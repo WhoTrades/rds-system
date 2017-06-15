3ee92b1a2b272e1aeb18df965494313c12149770
Artem Naumenko <an@whotrades.org>
2017-06-15 08:57:46 -0400

    [+] config.local.php sync script moved to RDS UI #WTA-1176

--- src/lib/CommandExecutor.php
+++ src/lib/CommandExecutor.php
@@ -17,17 +17,29 @@ class CommandExecutor
 
     /**
      * @param string $command
+     * @param string[]|null $env - assiciated list of env vars for command
      * @return string
      * @throws CommandExecutorException
      */
-    public function executeCommand($command)
+    public function executeCommand($command, $env = null)
     {
-        $this->debugLogger->message("Executing `$command`");
-        exec($command, $output, $returnVar);
+        if (!empty($env)) {
+            $commandWithEnv = "(";
+            foreach ($env as $key => $val) {
+                $commandWithEnv .= "export $key=" . escapeshellarg($val) . "; ";
+            }
+            $commandWithEnv .= $command . ")";
+        } else {
+            $commandWithEnv = $command;
+        }
+
+
+        $this->debugLogger->message("Executing `$commandWithEnv`");
+        exec($commandWithEnv, $output, $returnVar);
         $text = implode("\n", $output);
 
         if ($returnVar) {
-            throw new CommandExecutorException($command, "Return var is non-zero, code=$returnVar, command=$command", $returnVar, $text);
+            throw new CommandExecutorException($commandWithEnv, "Return var is non-zero, code=$returnVar, command=$commandWithEnv", $returnVar, $text);
         }
 
         return $text;
